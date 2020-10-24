8119983bbd0954f0af35ae564802b7d4af2394df
James Lungu <james.lungu@stonybrook.edu>
2020-10-24 18:03:44 -0400

    Reverted to Java 8 because its the superior version

--- server/src/main/java/com/panthers/main/PanthersApplication.java
+++ server/src/main/java/com/panthers/main/PanthersApplication.java
@@ -1,4 +1,4 @@
-package com.panthers.overview;
+package com.panthers.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
