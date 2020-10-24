5ec50d4a1c50315422700b9aeebf29c9b9ed50ec
James Lungu <james.lungu@stonybrook.edu>
2020-10-24 15:02:00 -0400

    Added server-side sleketon code for Spring Boot:

---
+++ server/src/main/java/com/panthers/overview/PanthersApplication.java
@@ -0,0 +1,13 @@
+package com.panthers.overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanthersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanthersApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/panthers/overview/PanthersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panthers.overview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanthersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
