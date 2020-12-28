5645cc14332468b1c21d33b67dffbb5d9dc4434c
James Lungu <54593148+jlungu@users.noreply.github.com>
2020-12-28 18:45:35 -0500

    Delete transferDataToSeaWulf.sh

--- server/src/main/resources/static/transferDataToSeaWulf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect
-
-spawn scp swData_job14.txt panthersm.slurm ../../../../../algorithm/algorithm.py ../../../../../algorithm/Cluster.py ../../../../../algorithm/Graph.py ../../../../../algorithm/Node.py ../../../../../algorithm/Subgraph.py ../../../../../algorithm/Precinct.py ../../../../../algorithm/Request.py jlungu@login.seawulf.stonybrook.edu:/gpfs/scratch/jlungu/PA_job#14
-expect "jlungu@login.seawulf.stonybrook.edu's password:"
-send "Ifyougiveapigapancake12112!"
-interact
